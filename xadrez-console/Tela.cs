07642626f9035196f11b2b3d353b33c172fd7a3c
Werner1201 <werner.romling@unigranrio.br>
2020-02-13 10:48:59 -0300

    Tela e Impressoes

---
+++ xadrez-console/Tela.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    class Tela
+    {
+        public static void ImprimirTabuleiro(Tabuleiro tab)
+        {
+            for (int i = 0; i < tab.Linhas; i++)
+            {
+                for (int j = 0; j < tab.Colunas; j++)
+                {
+                    Peca p = tab.peca(i, j);
+                    if(p == null)
+                    {
+                        Console.Write("- ");
+                    }
+                    else
+                    {
+                        Console.Write($"{p} ");
+                    }
+                   
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
